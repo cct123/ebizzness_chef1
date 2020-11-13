eaa19011e76bd43e828216e4b6b79ed4bbf0a05e
cct123 <liuyez123@gmail.com>
2020-11-12 22:55:42 -0500

    change php recipe

--- ebizzness/recipes/php.rb
+++ ebizzness/recipes/php.rb
@@ -11,6 +11,5 @@ end
 %w{php72 php72-cli php72-common php72-fpm php72-mbstring php72-mysqlnd php72-pdo  php72-xml php72-curl php72-imagick}.each do |pkg|
   package pkg do
     action :install
-    notifies :reload, 'service[httpd]', :immediately
   end
 end
